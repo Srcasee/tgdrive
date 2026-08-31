aea64ad2ef3628b4e15c67d42eaf52c5f10350c5
青青子衿 <srcasese@gmail.com>
2026-08-31 09:49:11 +0800

    fix: align account login script with account-named sessions

--- login-account.sh
+++ login-account.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-ACCOUNT_NAME="${1:?用法: ./login-account.sh <session_name>}"
-PHONE="${2:?用法: ./login-account.sh <session_name> <phone>}"
+ACCOUNT_NAME="${1:?用法: ./login-account.sh <account_name> <phone>}"
+PHONE="${2:?用法: ./login-account.sh <account_name> <phone>}"
 
 SESSION_DIR="${TG_SESSION_DIR:-/data/accounts}"
 SESSION_PATH="${SESSION_DIR}/${ACCOUNT_NAME}"
@@ -13,6 +13,7 @@ echo "[LOGIN] phone   : ${PHONE}"
 
 docker compose run --rm \
   -e TG_PHONE="${PHONE}" \
-  -e TG_SESSION="${SESSION_PATH}" \
+  -e TG_SESSION_DIR="${SESSION_DIR}" \
+  -e TG_ACCOUNT_NAME="${ACCOUNT_NAME}" \
   telegram-drive \
   python3 /app/telegram/login.py
